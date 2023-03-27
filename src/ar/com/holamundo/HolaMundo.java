78dbefe5f91d9b42ea13a8eeb1e338cab218fec8
selgadis <selgadis24@gmail.com>
2023-03-27 19:44:30 -0300

    segunda prueba

--- src/ar/com/holamundo/HolaMundo.java
+++ src/ar/com/holamundo/HolaMundo.java
@@ -1,12 +1,12 @@
+
 package ar.com.holamundo;
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-		
+		System.out.println("segunda prueba");
 		System.out.println("primera prueba");
-		System.out.println("borrame");
 	}
 
 }
